d8f40cdb5f566dc021c40220e2bf3373b638d159
srg <skabass@yandex.ru>
2020-03-31 23:19:45 +0300

    refactor: change single quotes to backticks

--- src/main.js
+++ src/main.js
@@ -371,20 +371,20 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector('.main');
-const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, createSiteMenuTemplate(), 'beforeend');
-render(siteMainElement, createFilterTemplate(), 'beforeend');
-render(siteMainElement, createBoardTemplate(), 'beforeend');
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
+render(siteMainElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, createBoardTemplate(), `beforeend`);
 
-const taskListElement = siteMainElement.querySelector('.board__tasks');
-const boardElement = siteMainElement.querySelector('.board');
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+const boardElement = siteMainElement.querySelector(`.board`);
 
-render(taskListElement, createTaskEditTemplate(), 'beforeend');
+render(taskListElement, createTaskEditTemplate(), `beforeend`);
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate(), 'beforeend');
+  render(taskListElement, createTaskTemplate(), `beforeend`);
 }
 
-render(boardElement, createLoadMoreButtonTemplate(), 'beforeend');
+render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
