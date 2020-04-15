7f9660aa647e57df26004305fa4d9274e0433b57
srg <skabass@yandex.ru>
2020-04-15 23:17:50 +0300

    3.7 add generating a template for sorting

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,9 +1,30 @@
+const createSortingMarkup = (sorting) => {
+  const {type, name} = sorting;
+  return (
+    `<a href="#" class="board__filter" data-sort-type="${type}">${name}</a>`
+  );
+};
+
 export const createSortingTemplate = () => {
+  const sortTypes = [
+    {
+      type: `default`,
+      name: `SORT BY DEFAULT`
+    },
+    {
+      type: `date-up`,
+      name: `SORT BY DATE up`
+    },
+    {
+      type: `date-down`,
+      name: `SORT BY DATE down`
+    }];
+
+  const sortingMarkup = sortTypes.map((it) => createSortingMarkup(it)).join(`\n`);
+
   return (
     `<div class="board__filter-list">
-        <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
-        <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
-        <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
-      </div>`
+      ${sortingMarkup}
+    </div>`
   );
 };
