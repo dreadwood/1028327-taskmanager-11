2d7d22fc6a1728e2effe96fed4667f3168f2fb17
srg <skabass@yandex.ru>
2020-04-15 17:19:01 +0300

    3.3 separate off tasks component on data structure and template. Add connect mock for tasks

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import {createSortingTemplate} from './components/sorting.js';
 import {createTaskTemplate} from './components/task.js';
 import {createTaskEditTemplate} from './components/task-edit.js';
 import {createLoadMoreButtonTemplate} from './components/load-more.js';
-
+import {generateTasks} from './mock/task.js';
 import {generateFilters} from './mock/filter.js';
 
 const TASK_COUNT = 3;
@@ -20,6 +20,7 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 
 const filters = generateFilters();
+const tasks = generateTasks(TASK_COUNT);
 
 render(siteMainElement, createFilterTemplate(filters), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
@@ -28,10 +29,10 @@ const boardElement = siteMainElement.querySelector(`.board`);
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortingTemplate(), `afterbegin`);
-render(taskListElement, createTaskEditTemplate(), `beforeend`);
+render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate(), `beforeend`);
+for (let i = 1; i < tasks.length; i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
