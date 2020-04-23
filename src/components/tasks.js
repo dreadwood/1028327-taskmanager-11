76ff40b49247312d129f1f203e3be505ad5d7810
srg <skabass@yandex.ru>
2020-04-24 01:08:02 +0300

    4.1.5 pull out tasks from the board

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,23 +1,23 @@
 import {createElement} from "../utils.js";
 
-const createBoardTemplate = () => {
+const createTasksTemplate = () => {
   return (
-    `<section class="board container"></section>`
+    `<div class="board__tasks"></div>`
   );
 };
 
-export default class Board {
+export default class Tasks {
   constructor() {
     this._element = null;
   }
 
   getTemplate() {
-    return createBoardTemplate();
+    return createTasksTemplate();
   }
 
   getElement() {
     if (!this._element) {
-      this._element = createElement(createBoardTemplate());
+      this._element = createElement(this.getTemplate());
     }
 
     return this._element;
