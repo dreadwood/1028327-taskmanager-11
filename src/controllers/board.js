90cddf6a077cccc06b3af95b6bde52c5e1d76db3
srg <skabass@yandex.ru>
2020-05-01 02:05:53 +0300

    5.2.1 add sorting function

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,4 +1,4 @@
-import Sorting from '../components/filter/sorting.js';
+import Sorting, {SortTypes} from '../components/filter/sorting.js';
 import Task from '../components/task/task.js';
 import TasksContainer from '../components/task/tasks-container.js';
 import TaskEdit from '../components/task/task-edit.js';
@@ -8,42 +8,6 @@ import {render, replace, remove} from '../utils/render.js';
 
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
-
-const renderTask = (taskListElement, task) => {
-  const replaceTaskToEdit = () => {
-    replace(taskEditComponent, taskComponent);
-  };
-
-  const replaceEditToTask = () => {
-    replace(taskComponent, taskEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      evt.preventDefault();
-      replaceEditToTask();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const taskComponent = new Task(task);
-  taskComponent.setEditButtonClickHandler((evt) => {
-    evt.preventDefault();
-    replaceTaskToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  const taskEditComponent = new TaskEdit(task);
-  taskEditComponent.setSubmitHandler((evt) => {
-    evt.preventDefault();
-    replaceEditToTask();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(taskListElement, taskComponent);
-};
 
 export default class BoardController {
   constructor(container) {
@@ -55,36 +19,115 @@ export default class BoardController {
     this._loadMoreButtonComponent = new LoadMore();
   }
 
+  _renderTask(taskListElement, task) {
+    const replaceTaskToEdit = () => {
+      replace(taskEditComponent, taskComponent);
+    };
+
+    const replaceEditToTask = () => {
+      replace(taskComponent, taskEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+      if (isEscKey) {
+        evt.preventDefault();
+        replaceEditToTask();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    const taskComponent = new Task(task);
+    taskComponent.setEditButtonClickHandler((evt) => {
+      evt.preventDefault();
+      replaceTaskToEdit();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+    const taskEditComponent = new TaskEdit(task);
+    taskEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      replaceEditToTask();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+    render(taskListElement, taskComponent);
+  }
+
+  _renderTasks(taskListElement, tasks) {
+    tasks.forEach((task) => this._renderTask(taskListElement, task));
+  }
+
+  _getSortedTasks(tasks, sortType, from, to) {
+    let sortedTasks = [];
+    const showingTasks = tasks.slice();
+
+    switch (sortType) {
+      case SortTypes.get(`SORT BY DATE up`):
+        sortedTasks = showingTasks.sort((a, b) => a.dueDate - b.dueDate);
+        break;
+      case SortTypes.get(`SORT BY DATE down`):
+        sortedTasks = showingTasks.sort((a, b) => b.dueDate - a.dueDate);
+        break;
+      case SortTypes.get(`SORT BY DEFAULT`):
+        sortedTasks = showingTasks;
+        break;
+    }
+
+    return sortedTasks.slice(from, to);
+  }
+
   render(tasks) {
+    const renderLoadMoreButton = () => {
+      if (showingTasksCount > tasks.length) {
+        return;
+      }
+
+      render(containerElement, this._loadMoreButtonComponent);
+
+      this._loadMoreButtonComponent.setClickHandler(() => {
+        const prevTasksCount = showingTasksCount;
+        showingTasksCount += SHOWING_TASKS_COUNT_BY_BUTTON;
+        const sortedTasks = this._getSortedTasks(tasks, this._sortingComponent.getSortType(), prevTasksCount, showingTasksCount);
+
+        this._renderTasks(taskListElement, sortedTasks);
+
+        if (showingTasksCount > tasks.length) {
+          remove(this._loadMoreButtonComponent);
+        }
+      });
+    };
+
+    const containerElement = this._container.getElement();
     const isAllTasksArhived = tasks.every((task) => task.isArhive);
+
     if (isAllTasksArhived) {
-      render(this._container.getElement(), this._noTasksComponent);
+      render(containerElement, this._noTasksComponent);
       return;
     }
 
-    render(this._container.getElement(), this._sortingComponent);
-    render(this._container.getElement(), this._tasksContainerComponent);
+    render(containerElement, this._sortingComponent);
+    render(containerElement, this._tasksContainerComponent);
 
-    const taskListElement = this._container.getElement().querySelector(`.board__tasks`);
+    const taskListElement = this._tasksContainerComponent.getElement();
 
     let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-    tasks.slice(0, showingTasksCount).forEach((task) => {
-      renderTask(taskListElement, task);
-    });
+    this._renderTasks(taskListElement, tasks.slice(0, showingTasksCount));
 
-    render(this._container.getElement(), this._loadMoreButtonComponent);
+    renderLoadMoreButton();
 
-    this._loadMoreButtonComponent.setClickHandler(() => {
-      const prevTasksCount = showingTasksCount;
-      showingTasksCount += SHOWING_TASKS_COUNT_BY_BUTTON;
+    this._sortingComponent.setSortTypeChangeHandler((sortType) => {
+      showingTasksCount = SHOWING_TASKS_COUNT_BY_BUTTON;
 
-      tasks.slice(prevTasksCount, showingTasksCount).forEach((task) => {
-        renderTask(taskListElement, task);
-      });
+      const sortedTasks = this._getSortedTasks(tasks, sortType, 0, showingTasksCount);
 
-      if (showingTasksCount > tasks.length) {
-        remove(this._loadMoreButtonComponent);
-      }
+      taskListElement.innerHTML = ``;
+
+      showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+      this._renderTasks(taskListElement, sortedTasks);
+
+      renderLoadMoreButton();
     });
   }
 }
