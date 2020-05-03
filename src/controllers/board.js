6f289253b164d5450d4b12307e36e1ef115ed270
srg <skabass@yandex.ru>
2020-05-04 00:56:59 +0300

    6.1.2 add onDataChange function for update data

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -21,6 +21,7 @@ export default class BoardController {
     this._tasksContainerComponent = new TasksContainer();
     this._loadMoreButtonComponent = new LoadMore();
 
+    this._onDataChange = this._onDataChange.bind(this);
     this._onSortTypeChange = this._onSortTypeChange.bind(this);
     this._sortingComponent.setSortTypeChangeHandler(this._onSortTypeChange);
   }
@@ -41,7 +42,7 @@ export default class BoardController {
 
     const taskListElement = this._tasksContainerComponent.getElement();
 
-    const newTasks = this._renderTasks(taskListElement, tasks.slice(0, this._showingTasksCount));
+    const newTasks = this._renderTasks(taskListElement, tasks.slice(0, this._showingTasksCount), this._onDataChange);
     this._showedTaskControllers = this._showedTaskControllers.concat(newTasks);
 
     this._renderLoadMoreButton();
@@ -74,16 +75,17 @@ export default class BoardController {
 
     taskListElement.innerHTML = ``;
 
-    const newTasks = this._renderTasks(taskListElement, sortedTasks);
+    const newTasks = this._renderTasks(taskListElement, sortedTasks, this._onDataChange);
     this._showedTaskControllers = newTasks;
 
     this._renderLoadMoreButton();
   }
 
-  _renderTasks(taskListElement, tasks) {
+  _renderTasks(taskListElement, tasks, onDataChange) {
     return tasks.map((task) => {
-      const taskController = new TaskController(taskListElement);
+      const taskController = new TaskController(taskListElement, onDataChange);
       taskController.render(task);
+
       return taskController;
     });
   }
@@ -102,7 +104,7 @@ export default class BoardController {
       this._showingTasksCount += SHOWING_TASKS_COUNT_BY_BUTTON;
 
       const sortedTasks = this._getSortedTasks(this._tasks, this._sortingComponent.getSortType(), prevTasksCount, this._showingTasksCount);
-      const newTasks = this._renderTasks(taskListElement, sortedTasks);
+      const newTasks = this._renderTasks(taskListElement, sortedTasks, this._onDataChange);
 
       this._showedTaskControllers = this._showedTaskControllers.concat(newTasks);
 
@@ -110,5 +112,17 @@ export default class BoardController {
         remove(this._loadMoreButtonComponent);
       }
     });
+  }
+
+  _onDataChange(taskController, oldData, newData) {
+    const index = this._tasks.findIndex((it) => it === oldData);
+
+    if (index === -1) {
+      return;
+    }
+
+    this._tasks = [].concat(this._tasks.slice(0, index), newData, this._tasks.slice(index + 1));
+
+    taskController.render(this._tasks[index]);
   }
 }
--- src/controllers/task.js
+++ src/controllers/task.js
@@ -3,8 +3,9 @@ import TaskEdit from '../components/task/task-edit.js';
 import {render, replace} from '../utils/render.js';
 
 export default class TaskController {
-  constructor(container) {
+  constructor(container, onDataChange) {
     this._container = container;
+    this._onDataChange = onDataChange;
 
     this._taskComponent = null;
     this._taskEditComponent = null;
@@ -20,6 +21,20 @@ export default class TaskController {
       evt.preventDefault();
       this._replaceTaskToEdit();
       document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._taskComponent.setArchiveButtonClickHandler((evt) => {
+      evt.preventDefault();
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
+    });
+
+    this._taskComponent.setFavoritesButtonClickHandler((evt) => {
+      evt.preventDefault();
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite,
+      }));
     });
 
     this._taskEditComponent.setSubmitHandler((evt) => {
