dca68069b6f5aa9df1ee03972f21c6b9f4c1e6a1
srg <skabass@yandex.ru>
2020-05-27 21:54:11 +0300

    8.1.7 add check from server

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 import taskModel from './models/task-model.js';
 
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
 export default class API {
   constructor(authorization) {
     this._authorization = authorization;
@@ -10,6 +18,7 @@ export default class API {
     headers.append(`Authorization`, this._authorization);
 
     return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks`, {headers})
+      .then(checkStatus)
       .then((response) => response.json())
       .then(taskModel.parseTask);
   }
@@ -23,6 +32,7 @@ export default class API {
       body: JSON.stringify(data),
       headers,
     })
+      .then(checkStatus)
       .then((response) => response.json())
       .then(taskModel.parseTasks);
   }
