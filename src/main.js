1b1363c16ee63aa5c9f0d2eac952fbc5997cceef
srg <skabass@yandex.ru>
2020-05-27 20:39:11 +0300

    8.1.2 refactoring in main.js

--- src/main.js
+++ src/main.js
@@ -9,30 +9,31 @@ import {generateTasks} from './mock/task.js';
 
 const TASK_COUNT = 20;
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-const siteMenuComponent = new SiteMenuComponent();
-render(siteHeaderElement, siteMenuComponent);
-
-const tasks = generateTasks(TASK_COUNT);
-const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
-
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
-
-const boardComponent = new BoardComponent();
-render(siteMainElement, boardComponent);
-const boardController = new BoardController(boardComponent, tasksModel);
-boardController.render();
-
 const dateTo = new Date();
 const dateFrom = (() => {
   const d = new Date(dateTo);
   d.setDate(d.getDate() - 7);
   return d;
 })();
+
+const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+
+tasksModel.setTasks(tasks);
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuComponent();
 const statisticsComponent = new StatisticsComponent(tasksModel, dateFrom, dateTo);
+
+const boardComponent = new BoardComponent();
+const boardController = new BoardController(boardComponent, tasksModel);
+const filterController = new FilterController(siteMainElement, tasksModel);
+
+render(siteHeaderElement, siteMenuComponent);
+filterController.render();
+render(siteMainElement, boardComponent);
+boardController.render();
 render(siteMainElement, statisticsComponent);
 statisticsComponent.hide();
 
