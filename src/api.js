fceecd1d571205475f3bd269be291493f2ac3555
srg <skabass@yandex.ru>
2020-05-28 02:06:00 +0300

    8.2.2 add deleteTask method in API

--- src/api.js
+++ src/api.js
@@ -51,6 +51,13 @@ export default class API {
       .then(TaskModel.parseTask);
   }
 
+  deleteTask(id) {
+    return this._load({
+      url: `tasks/${id}`,
+      method: Method.DELETE,
+    });
+  }
+
   _load({url, method = Method.GET, body = null, headers = new Headers()}) {
     headers.append(`Authorization`, this._authorization);
 
